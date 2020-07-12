a22b0a60c4de76a4ed99e1ed3a97b4b9d8f1b5d6
Luca Stephan Kohls <info@luca-kohls.net>
2020-07-12 15:00:08 +0200

    update

---
+++ support/_config/config.php
@@ -0,0 +1,24 @@
+<?php
+
+/*********************************************************************************
+* LiveZilla config.php
+*
+* Copyright 2016 LiveZilla GmbH
+* All rights reserved.
+* LiveZilla is a registered trademark.
+*
+********************************************************************************/
+
+$_CONFIG['gl_pr_cr'] = '1590012336';
+$_CONFIG['gl_lzid'] = 'baef9f7c76';
+$_CONFIG['b64'] = false;
+
+$_CONFIG[0]['gl_db_host'] = 'localhost';
+$_CONFIG[0]['gl_db_user'] = 'lucakohls';
+$_CONFIG[0]['gl_db_ext'] = 'mysqli';
+$_CONFIG[0]['gl_db_eng'] = 'InnoDB';
+$_CONFIG[0]['gl_db_pass'] = 'mAqe3GKuOZ4!';
+$_CONFIG[0]['gl_db_name'] = 'livezilla';
+$_CONFIG[0]['gl_db_prefix'] = 'lz_';
+
+?>
